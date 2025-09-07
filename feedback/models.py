afd925c32b55603dc0bbc18826b9bf71ee69e4bf
DSkilton <duncanskilton.ds@gmail.com>
2025-09-07 18:47:24 +0100

    add Module and Assignment Module

--- feedback/models.py
+++ feedback/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from .db import Base
 from . import models
 
@@ -11,3 +11,19 @@ class User(Base):
     role = Column(String, nullable=False)
     password_hash = Column(String, nullable=False)
 
+class Module(Base):
+    __tablename__ = "modules"
+
+    id = Column(Integer, primary_key=True)
+    mod_number = Column(String, unique=True)
+    title = Column(String)
+    spec = Column(String)
+
+class Assignment(Base):
+    __tablename__ = "assignments"
+
+    id = Column(Integer, primary_key=True)
+    module_id = Column(Integer, ForeignKey("modules.id"))
+    title = Column(String)
+    description = Column(String)
+    due_date = Column(DateTime)
